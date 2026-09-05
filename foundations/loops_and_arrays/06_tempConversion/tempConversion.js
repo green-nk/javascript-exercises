d422a10bf54dba38d60fb9b054ffb51b229da140
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-05 12:56:57 +0700

    Test convertToCelsius

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+/*
+Formula
+C/5 = (F-32)/9
+*/
+
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 9 * 5;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (9 * celsius / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
