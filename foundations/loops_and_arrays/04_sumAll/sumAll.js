f80f14ed0dfb1e9435860600a2d1cc6313aec083
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-05 12:30:52 +0700

    Finish sumAll exercise
    `fromNum` and `toNum` need to be no constant since inputs can be in any
    order and needed to be ready for an adjustment.

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!isValidInput(a) || !isValidInput(b)) return "ERROR";
 
+    let fromNum = a;
+    let toNum = b;
+    if (a > b) {
+        fromNum = b;
+        toNum = a;
+    }
+
+    let result = 0;
+    for (let num = fromNum; num <= toNum; num++) {
+        result += num;
+    }
+
+    return result;
 };
+
+function isValidInput(inp) {
+    return Number.isInteger(inp) && inp >= 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
