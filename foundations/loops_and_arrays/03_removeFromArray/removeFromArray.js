f7ca9907ebfc6e689c4a504988cc436f9deb5e6c
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-05 12:10:59 +0700

    Finish removeFromArray exercise

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        const elem = args[i];        
+
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === elem) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
