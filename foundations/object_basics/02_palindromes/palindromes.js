e5ed8ed616571c56ac12f6ee9559aea489da3b76
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-16 21:01:13 +0700

    Draft simplest palindrome detector

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    for (let i = 0, j = string.length - 1; j >= i; i++, j--) {
+        if (string.at(i) != string.at(j)) return false;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
--- foundations/object_basics/02_palindromes/palindromes.spec.js
+++ foundations/object_basics/02_palindromes/palindromes.spec.js
@@ -4,10 +4,10 @@ describe('palindromes', () => {
   test('detects odd-length palindrome', () => {
     expect(palindromes('racecar')).toBe(true);
   });
-  test.skip('detects even-length palindrome', () => {
+  test('detects even-length palindrome', () => {
     expect(palindromes('abba')).toBe(true);
   });
-  test.skip('detects palindrome with numbers', () => {
+  test('detects palindrome with numbers', () => {
     expect(palindromes('rac3e3car')).toBe(true);
   });
   test.skip('detects palindrome with multiple words', () => {
