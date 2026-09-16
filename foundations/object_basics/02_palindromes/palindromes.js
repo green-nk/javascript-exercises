100d02f1884314416f49d3b60d91b3c10deb5607
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-16 21:23:56 +0700

    Handle punctuations

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,10 +1,23 @@
 const palindromes = function (string) {
-    for (let i = 0, j = string.length - 1; j >= i; i++, j--) {
-        if (string.at(i) != string.at(j)) return false;
+    const lowerString = string.toLowerCase();
+
+    for (let i = 0, j = lowerString.length - 1; j >= i; i++, j--) {
+        let s1 = lowerString.at(i);
+        let s2 = lowerString.at(j);
+        
+        while (!isLower(s1)) s1 = lowerString.at(++i);
+        while (!isLower(s2)) s2 = lowerString.at(--j);
+
+        if (i < j && (s1 != s2)) return false;
     }
 
     return true;
 };
+
+function isLower(s) {
+    const c = s.charCodeAt(0);
+    return c >= 97 && c <= 122; // a-z
+}
 
 // Do not edit below this line
 module.exports = palindromes;
