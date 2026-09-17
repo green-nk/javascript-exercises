ad0bf92c4c0918116603a75a3a81aac578106acc
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-17 04:11:17 +0000

    Check input to be valid fibo number

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (!isValidInt(number)) return "OOPS";
+};
 
+function isValidInt(input) {
+    const number = +input;
+    return Number.isInteger(number) && number > 0;
 };
 
 // Do not edit below this line
