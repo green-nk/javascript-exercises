703374d2ef1ad70010dcb40ccbbaa5265fca8e99
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-17 05:06:30 +0000

    Test with no yearOfDeath field

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -3,7 +3,13 @@ const findTheOldest = function(people) {
     let greatestAgeIdx = -1;
 
     for (let i = 0; i < people.length; i++) {
-        const age = people[i].yearOfDeath - people[i].yearOfBirth;
+        const person = people[i];
+
+        const birthyear = person.yearOfBirth;
+        let deathyear = new Date().getFullYear();
+        if ("yearOfDeath" in person) deathyear = person.yearOfDeath;
+
+        const age = deathyear - birthyear;
         if (age > greatestAge) {
             greatestAge = age;
             greatestAgeIdx = i;
