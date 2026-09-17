ff36809b288ed784c90a571ba0cbe48857ebc45e
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-17 04:56:25 +0000

    Loop to find the oldest person

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let greatestAge = Number.MIN_SAFE_INTEGER;
+    let greatestAgeIdx = -1;
 
+    for (let i = 0; i < people.length; i++) {
+        const age = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (age > greatestAge) {
+            greatestAge = age;
+            greatestAgeIdx = i;
+        }
+    }
+
+    return people[greatestAgeIdx];
 };
 
 // Do not edit below this line
