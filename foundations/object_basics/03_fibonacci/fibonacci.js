0a26d88851c34a7ba9bf37574fb57895466ab620
Nakarin Korsuwan <80184132+green-nk@users.noreply.github.com>
2026-09-17 04:28:03 +0000

    Pass fibo test on long sequence

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = function(idx) {
     if (!isValidInt(idx)) return "OOPS";
+
+    // 0, 1, 1, 2, 3, 5, 8, ...
+    let fiboSeq = [0, 1];
+    for (let i = 2; i <= idx; i++) {
+        const num = fiboSeq[i - 1] + fiboSeq[i - 2];
+        fiboSeq.push(num);
+    }
+ 
+    return fiboSeq.at(idx);
 };
 
 function isValidInt(input) {
